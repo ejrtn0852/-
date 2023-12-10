b1b2e15623eaaf7daf3e6b726fbd1ca33fec8bb3
dog <ejrtn0852@naver.com>
2023-12-11 02:55:19 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,15 +1,9 @@
 function solution(n) {
-  let totalPieces = 6; // 여섯 조각으로 나눠진 피자
+  let totalPieces = 6; 
   let count = 0;
-  let i = true
-  while (i) {
-    if(totalPieces % n !== 0) {
-      totalPieces += 6;
-      count++;   
-    } else {
-        count++;
-        i = false;
-    }
+  while (totalPieces % n !== 0) {
+    totalPieces += 6;
+    count++;
   }
-  return count;
+  return count + 1; 
 }
