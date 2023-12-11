cc6988986d814a0f05907a07883a9ced148d99f9
dog <ejrtn0852@naver.com>
2023-12-11 16:49:59 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,4 +1,12 @@
-function solution(num_list) {
-    var answer = [...num_list].reverse()
-    return answer
+// function solution(num_list) {
+//     var answer = [...num_list].reverse()
+//     return answer
+// }
+
+const solution = (num_list) => {
+  let array = [];
+    for(let i =num_list.length -1; i>=0 ; i--) {
+        array.push(num_list[i])
+    }
+    return array
 }
