179eca187f855d52dfe2470e65b79fca5be5fb13
dog <ejrtn0852@naver.com>
2023-12-21 19:48:53 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,25 +1,37 @@
-const findMaxKeys = (obj) => {
-    const entries = Object.entries(obj);
-    const maxValue = Math.max(...Object.values(obj));
+// const findMaxKeys = (obj) => {
+//     const entries = Object.entries(obj);
+//     const maxValue = Math.max(...Object.values(obj));
+//     return entries.filter(([key, value]) => value === maxValue).map(([key]) => key);
+// };
 
-    return entries.reduce((acc, [key, value]) => {
-        if (value === maxValue) {
-            acc.push(key);
-        }
-        return acc;
-    }, []);
-};
+// function solution(array) {
+//     const obj = {};
+//     array.forEach((item) => !obj[item] ? obj[item] = 1 : obj[item]++);
+//     let result = findMaxKeys(obj);
+//     if(result.length > 1) {
+//         return -1
+//     } 
+//     return Number(result)
+// }
+
+
 
 function solution(array) {
-    const obj = {};
-    array.forEach((item) => !obj[item] ? obj[item] = 1 : obj[item]++);
-    let result = findMaxKeys(obj);
-    if(findMaxKeys(obj).length > 1) {
-        return -1
-    } 
-    return Number(result)
-    // return findMaxKeys(obj).length > 1 ? -1 : 1;
+    const frequencyMap = {};
+    let maxFrequency = 0;
+    let maxItem = null;
+    let maxItemCount = 0;
+
+    for (const item of array) {
+        frequencyMap[item] = (frequencyMap[item] || 0) + 1;
+        if (frequencyMap[item] > maxFrequency) {
+            maxFrequency = frequencyMap[item];
+            maxItem = item;
+            maxItemCount = 1;
+        } else if (frequencyMap[item] === maxFrequency) {
+            maxItemCount++;
+        }
+    }
+
+    return maxItemCount === 1 ? Number(maxItem) : -1;
 }
-
-
-
