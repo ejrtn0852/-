53691032379a14488bbe278639c9c293d199b531
dog <ejrtn0852@naver.com>
2024-01-03 05:30:30 +0900

    [level unrated] Title: 합성수 찾기, Time: 0.05 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    // 에라토스테네스의 체 알고리즘(큰수에 적합)
+    // 2부터 시작하는 연속된 정수 목록 생성
+    let isPrime = Array(n + 1).fill(true);
+    isPrime[0] = isPrime[1] = false;
+    
+    for(let i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    let compositeNumbers = [];
+    
+    for(let i = 2; i <= n; i++) {
+        if(!isPrime[i]) {
+            compositeNumbers.push(i);
+        }
+    }
+    return compositeNumbers.length
+}
