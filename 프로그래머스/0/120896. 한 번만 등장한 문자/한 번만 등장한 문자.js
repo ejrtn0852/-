68168b5d0df4d1230643f83167ba1d68182de572
dog <ejrtn0852@naver.com>
2024-02-02 10:04:52 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,11 +1,7 @@
 function solution(s) {
     let obj = {};
-    for (let i = 0; i < s.length; i++) {
-        if (!obj.hasOwnProperty(s[i])) {
-            obj[s[i]] = 1; 
-        } else {
-            obj[s[i]] += 1; 
-        }
+    for(let key of s) {
+        obj[key] = (obj[key] || 0) + 1;
     }
     let result = Object.keys(obj).filter(key => obj[key] === 1)
     return !result ? "" : result.sort().join("")
